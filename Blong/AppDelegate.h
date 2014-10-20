e978fc17ce945f5d534f9bb1c4d66547af83a150
Lisa Accardi <lisa@lemonappstand.com>
2014-10-20 13:31:22 -0400

    Initial Commit

---
+++ Blong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blong
+//
+//  Created by Lisa Accardi on 10/20/14.
+//  Copyright (c) 2014 BLOC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blong
+//
+//  Created by Lisa Accardi on 10/20/14.
+//  Copyright (c) 2014 BLOC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
